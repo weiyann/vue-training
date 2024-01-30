1dc85b99921d616e7493047a2ea0dee25199442c
weiyann <jona11105@gmail.com>
2024-01-30 16:42:22 +0800

    first Vue App

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+// 載入 createApp 函式
+import { createApp } from "vue";
+// 載入根組件
+import App from "./App.vue";
+// 建立 Vue App 物件
+const app = createApp(App);
+// 掛載到 HTML 標籤底下
+app.mount("#app");
