b36b3d7cad430c647129717e67e3c660b8b6463f
weiyann <jona11105@gmail.com>
2024-02-01 01:18:50 +0800

    execution practice

--- src/views/ExecutionView.vue
+++ src/views/ExecutionView.vue
@@ -1,5 +1,31 @@
-<script setup></script>
+<script setup>
+let level = 0;
+let topics = ["HTML", "CSS", "JavaScript", "Vue.js"];
+let objects = { html: "HTML", css: "CSS", js: "JavaScript", vue: "Vue.js" };
+</script>
 <template>
   <h1>樣板語法的判斷式與迴圈</h1>
+  <main>
+    <div v-if="level === 3">大神</div>
+    <div v-else-if="level === 2">資深</div>
+    <div v-else-if="level === 1">中階</div>
+    <div v-else>
+      <div>入門</div>
+      <!-- for 迴圈-陣列 -->
+      <ul>
+        <li v-for="name in topics">{{ name }}</li>
+      </ul>
+
+      <!-- for 迴圈-陣列索引 -->
+      <ul>
+        <li v-for="(name, index) in topics">{{ index }}-{{ name }}</li>
+      </ul>
+
+      <!-- for 迴圈-物件 -->
+      <ul>
+        <li v-for="(value, key) in objects">{{ key }}-{{ value }}</li>
+      </ul>
+    </div>
+  </main>
 </template>
 <style scoped></style>
