6d5d67dcf1c9c1070417e2d7ef2e15567474f700
weiyann <jona11105@gmail.com>
2024-01-31 15:00:48 +0800

    樣板語法：標籤內文與屬性

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,42 @@
 <script setup>
-console.log("我的第一個組件");
+let message = "Hello Vue";
+let googleLink = "<a href='https://google.com'>Google</a>";
+let name = "content";
+let checked = false;
 </script>
 
 <template>
-  <nav>導覽列</nav>
-  <main>主內容</main>
+  <main>
+    <!-- 嵌入純文字 -->
+    <div>{{ message }}</div>
+
+    <!-- 嵌入 HTML 語法 -->
+    <div v-html="googleLink"></div>
+
+    <!-- 嵌入屬性值 -->
+    <div v-bind:class="name">嵌入屬性值</div>
+    <div :class="name">嵌入屬性值-簡寫</div>
+
+    <!-- 透過程式語句產生標籤內文 -->
+    <div v-html="message.toUpperCase()"></div>
+    <div>{{ message.toUpperCase() }}</div>
+
+    <!-- 透過程式語句產生標籤屬性值 -->
+    <div :class="checked ? 'light' : 'dark'">操作標籤的屬性</div>
+  </main>
 </template>
 
 <style scoped>
 main {
   background-color: #ddd;
+}
+.content {
+  color: blue;
+}
+.light {
+  color: #888888;
+}
+.dark {
+  color: #262626;
 }
 </style>
